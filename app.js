03c15aae4efe7b12535881605db38d7d3a0c9692
srinadh ghattamneni <srinadhghattamneni1990@gmail.com>
2024-10-18 00:24:39 +0530

    first commit

--- app.js
+++ app.js
@@ -5,6 +5,15 @@ const Listing = require("./models/listing.js");
 const path = require("path");
 const methodOverride = require("method-override");
 
+const ejsMate = require("ejs-mate");
+const wrapAsync = require("./utils/wrapAsync.js");
+const ExpressError = require("./utils/ExpressError.js");
+const { errorMonitor } = require("events");
+// const { listingSchema,reviewSchema } = require("./schema.js");
+const { listingSchema } = require("./schema.js");
+
+// const Review =require("./models/review.js");
+
 const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
 
 main()
@@ -23,57 +32,131 @@ app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
+// use ejs-locals for all ejs templates:
+app.engine('ejs', ejsMate);
+app.use(express.static(path.join(__dirname, "/public")));
+
 
 app.get("/", (req, res) => {
   res.send("Hi, I am root");
 });
 
+const validateListing = (req, res, next) => {
+  const { error } = listingSchema.validate(req.body);
+  if (error) {
+    const errMsg = error.details.map((el) => el.message).join(". ");
+    throw new ExpressError(400, errMsg);
+  } else {
+    next();
+  }
+};
+
+// const validateReview = (req, res, next) => {
+//   let { error } = reviewSchema.validate(req.body);
+//   if (error) {
+//     let { errMsg } = error.details.map((el) => el.message).join(".");
+//     throw new ExpressError(400, errMsg);
+//   } else {
+//     next();
+//   }
+// }
+
 //Index Route
-app.get("/listings", async (req, res) => {
+app.get("/listings", wrapAsync(async (req, res) => {
   const allListings = await Listing.find({});
   res.render("listings/index.ejs", { allListings });
-});
+}));
 
 //New Route
 app.get("/listings/new", (req, res) => {
   res.render("listings/new.ejs");
 });
 
-//Show Route
-app.get("/listings/:id", async (req, res) => {
-  let { id } = req.params;
-  const listing = await Listing.findById(id);
-  res.render("listings/show.ejs", { listing });
-});
+// //Show Route with reviews
+// app.get("/listings/:id", wrapAsync(async (req, res) => {
+//   let { id } = req.params;
+//   const listing = await Listing.findById(id).populate("reviews");
+//   res.render("listings/show.ejs", { listing });
+// }));
 
-//Create Route
-app.post("/listings", async (req, res) => {
-  const newListing = new Listing(req.body.listing);
+// Show Route without reviews
+app.get("/listings/:id", wrapAsync(async (req, res) => {
+  let { id } = req.params;
+  const listing = await Listing.findById(id);  // Remove populate('reviews')
+  res.render("listings/show.ejs", { listing });
+}));
+
+
+// //Create Route
+// app.post("/listings", async (req, res) => {
+//   const newListing = new Listing(req.body.listing);
+//   await newListing.save();
+//   res.redirect("/listings");
+// });
+
+// Create Route modifed to accept the coorect image format 
+app.post("/listings", validateListing, wrapAsync(async (req, res, next) => {
+
+  const { listing } = req.body;
+  listing.image.filename = "listingimage";  // Set the filename directly
+  const newListing = new Listing(listing);
   await newListing.save();
   res.redirect("/listings");
-});
+
+
+
+}));
+
 
 //Edit Route
-app.get("/listings/:id/edit", async (req, res) => {
+app.get("/listings/:id/edit", wrapAsync(async (req, res) => {
   let { id } = req.params;
   const listing = await Listing.findById(id);
   res.render("listings/edit.ejs", { listing });
-});
+}));
 
-//Update Route
-app.put("/listings/:id", async (req, res) => {
-  let { id } = req.params;
-  await Listing.findByIdAndUpdate(id, { ...req.body.listing });
+// //Update Route
+// app.put("/listings/:id", async (req, res) => {
+//   let { id } = req.params;
+//   await Listing.findByIdAndUpdate(id, { ...req.body.listing });
+//   res.redirect(`/listings/${id}`);
+// });
+
+
+// Update Route modifed to correctly store the image as object
+app.put("/listings/:id", validateListing, wrapAsync(async (req, res) => {
+  const { id } = req.params;
+  const { listing } = req.body;
+  listing.image.filename = "listingimage";  // Set the filename directly
+  await Listing.findByIdAndUpdate(id, { ...listing });
   res.redirect(`/listings/${id}`);
-});
+}));
+
 
 //Delete Route
-app.delete("/listings/:id", async (req, res) => {
+app.delete("/listings/:id", wrapAsync(async (req, res) => {
   let { id } = req.params;
   let deletedListing = await Listing.findByIdAndDelete(id);
   console.log(deletedListing);
   res.redirect("/listings");
-});
+}));
+
+//Reviews
+//Reviews Route
+// app.post("/listings/:id/reviews",validateReview, wrapAsync(async (req, res) => {
+//   let listing = await Listing.findById(req.params.id);
+//   let newReview = new Review(req.body.review);
+
+//   listing.reviews.push(newReview);
+
+//   await newReview.save();
+//   await listing.save();
+
+//   // console.log("new review created");
+//   // res.send("new review created");
+//   res.redirect(`/listings/${listing._id}`);
+// }));
+
 
 // app.get("/testListing", async (req, res) => {
 //   let sampleListing = new Listing({
@@ -88,6 +171,16 @@ app.delete("/listings/:id", async (req, res) => {
 //   console.log("sample was saved");
 //   res.send("successful testing");
 // });
+
+app.all("*", (req, res, next) => {
+  next(new ExpressError(404, "Page not Found !"));
+});
+
+app.use((err, req, res, next) => {
+  let { statusCode = 500, message = "something went wrong !" } = err;
+  res.status(statusCode).render("error.ejs", { err });
+  // res.status(statusCode).send(message);
+});
 
 app.listen(8080, () => {
   console.log("server is listening to port 8080");
---
+++ schema.js
@@ -0,0 +1,24 @@
+const Joi = require("joi");
+
+//Schema for listings
+module.exports.listingSchema = Joi.object({ 
+    listing: Joi.object({
+        title: Joi.string().required(),
+        description: Joi.string().required(),
+        location: Joi.string().required(),
+        country: Joi.string().required(),
+        price: Joi.number().required().min(0),
+        image: Joi.object({
+            filename: Joi.string().allow(null, ''),  // filename is optional
+            url: Joi.string().allow(null, ''),       // url is optional and can be null or an empty string
+        }).required(),  // image object is still required
+    }).required(),
+});
+
+
+// module.exports.reviewSchema = Joi.object({
+//     review: Joi.object({
+//         rating: Joi.number().required().min(1).max(5),
+//         comment: Joi.string().required(),
+//     }).required(),
+// });
